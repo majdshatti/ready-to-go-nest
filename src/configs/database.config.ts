04b218195473bf7ff3c0b3661d9a01ffd99021d8
majdshatti <majdshatti8@gmail.com>
2023-09-21 00:56:17 +0300

    feat: add datasoruce options

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -1,15 +1,26 @@
 import { registerAs } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config as dotenvConfig } from 'dotenv';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+dotenvConfig({ path: '.env' });
 
 /**
  * Registering configurations in the name of database
  *
  */
-export default registerAs('database', () => ({
+const config: DataSourceOptions = {
+  type: 'mysql',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   name: process.env.DB_NAME,
-  engine: process.env.DB_HOST,
-  synchronize: process.env.APP_ENV === 'development' ? true : false,
-}));
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  namingStrategy: new SnakeNamingStrategy(),
+};
+
+export default registerAs('database', () => config);
+export const connectionSource = new DataSource(config as DataSourceOptions);
