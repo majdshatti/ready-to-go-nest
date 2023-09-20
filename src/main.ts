a5f56c10bc3211f49b0b65f659ba55e5c4eb9786
majdshatti <majdshatti8@gmail.com>
2023-09-21 00:54:35 +0300

    feat: add logger and exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor } from '@nestjs/common';
+import { ClassSerializerInterceptor, VersioningType } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import { loggerOptions } from './configs/logger.config';
+import { AllExceptionsFilter } from './exceptions';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger(loggerOptions),
+  });
+  const httpAdapter = app.get(HttpAdapterHost);
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  // app.enableVersioning({
+  //   type: VersioningType.URI,
+  //   defaultVersion: '1',
+  // });
 
   await app.listen(3001);
 }
