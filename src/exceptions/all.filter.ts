256581f13bfb734acd1cee1d4ce025f415e43f7d
majdshatti <majdshatti8@gmail.com>
2023-09-22 21:29:06 +0300

    feat: add sending server error mail & logging error

--- src/exceptions/all.filter.ts
+++ src/exceptions/all.filter.ts
@@ -1,17 +1,28 @@
+import { InjectQueue } from '@nestjs/bull';
 import {
   ExceptionFilter,
   Catch,
   ArgumentsHost,
   HttpException,
   HttpStatus,
+  InternalServerErrorException,
+  Logger,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { HttpAdapterHost } from '@nestjs/core';
+import { Queue } from 'bull';
+import { IMailError } from 'src/mail/interfaces/error.interface';
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
-  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+  constructor(
+    private readonly httpAdapterHost: HttpAdapterHost,
+    @InjectQueue('error') private readonly errorQueue: Queue,
+    private readonly configService: ConfigService,
+    private readonly logger: Logger,
+  ) {}
 
-  catch(exception: unknown, host: ArgumentsHost): void {
+  async catch(exception: unknown, host: ArgumentsHost): Promise<void> {
     const { httpAdapter } = this.httpAdapterHost;
 
     const ctx = host.switchToHttp();
@@ -27,6 +38,34 @@ export class AllExceptionsFilter implements ExceptionFilter {
       path: httpAdapter.getRequestUrl(ctx.getRequest()),
     };
 
+    if (
+      exception instanceof InternalServerErrorException &&
+      this.configService.get('app.env') === 'production'
+    ) {
+      const error: IMailError = {
+        message: exception.message,
+        dateTime: responseBody.timestamp,
+        code: httpStatus,
+      };
+
+      await this.errorQueue.add(error);
+    }
+
+    if (
+      this.hasMessageProperty(exception) &&
+      this.hasStackProperty(exception)
+    ) {
+      this.logger.error(exception.message, exception.stack);
+    }
+
     httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+
+  hasMessageProperty(obj: any): obj is { message: string } {
+    return typeof obj === 'object' && 'message' in obj;
+  }
+
+  hasStackProperty(obj: any): obj is { stack: string } {
+    return typeof obj === 'object' && 'stack' in obj;
   }
 }
