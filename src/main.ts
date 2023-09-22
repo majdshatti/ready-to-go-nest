31dec196029368cb3ae06a104aa32bd09504020d
majdshatti <majdshatti8@gmail.com>
2023-09-22 21:28:24 +0300

    refactor: move using of filter to app

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,15 @@ import { ClassSerializerInterceptor, VersioningType } from '@nestjs/common';
 import { WinstonModule } from 'nest-winston';
 import { loggerOptions } from './configs/logger.config';
 import { AllExceptionsFilter } from './exceptions';
+import { MailService } from './mail/mail.service';
+import { MailerService } from '@nestjs-modules/mailer';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: WinstonModule.createLogger(loggerOptions),
   });
-  const httpAdapter = app.get(HttpAdapterHost);
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   // app.enableVersioning({
   //   type: VersioningType.URI,
