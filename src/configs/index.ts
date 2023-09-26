67b04624741afd452d94e8caa9ecc671cb6e7ca2
majdshatti <majdshatti8@gmail.com>
2023-09-27 01:09:19 +0300

    feat: add mail conf to exporter

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,5 +2,6 @@ import databaseConfig from './database.config';
 import appConfig from './app.config';
 import keysConfig from './keys.config';
 import googleConfig from './google.config';
+import mailConfig from './mail.config';
 
-export { databaseConfig, appConfig, keysConfig, googleConfig };
+export { databaseConfig, appConfig, keysConfig, googleConfig, mailConfig };
