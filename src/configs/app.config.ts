3aed3ecd6148a7ef51c372a1d30cc03062cece29
majdshatti <majdshatti8@gmail.com>
2023-09-27 01:08:46 +0300

    fix: wrong env keys

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -5,8 +5,8 @@ import { registerAs } from '@nestjs/config';
  *
  */
 export default registerAs('app', () => ({
-  env: process.env.development,
-  name: process.env.tasharuk,
+  env: process.env.APP_ENV,
+  name: process.env.APP_NAME,
   url: process.env.APP_URL,
   port: process.env.APP_PORT,
 }));
