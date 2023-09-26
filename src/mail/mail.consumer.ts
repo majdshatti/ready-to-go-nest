40e16b18bed49daf56c2ad966ff2214dc8fc43de
majdshatti <majdshatti8@gmail.com>
2023-09-27 01:15:13 +0300

    feat: add storing for 500 exception

--- src/mail/mail.consumer.ts
+++ src/mail/mail.consumer.ts
@@ -2,16 +2,26 @@ import { Process, Processor } from '@nestjs/bull';
 import { Job } from 'bull';
 import { MailService } from './mail.service';
 import { IMailError } from './interfaces/error.interface';
+import { LogService } from 'src/modules/log';
 
-@Processor('mail')
+@Processor('error')
 export class MailConsumer {
-  constructor(private readonly mailService: MailService) {}
+  constructor(
+    private readonly mailService: MailService,
+    private readonly logService: LogService,
+  ) {}
 
   @Process('internal_error_occurred')
-  async sendMail(job: Job<IMailError>) {
-    const mailData: IMailError = job.data;
+  async sendException(job: Job<IMailError>) {
+    const error: IMailError = job.data;
 
-    await this.mailService.sendException(mailData);
+    await this.logService.save({
+      message: error.message,
+      statusCode: error.code,
+      path: error.path,
+    });
+
+    await this.mailService.sendException(error);
 
     return {};
   }
