32a6b2bd07d2fa2a08f10d78d74ac6f8223fecf7
majdshatti <majdshatti8@gmail.com>
2023-09-29 21:43:21 +0300

    feat: remove exported repo

--- src/modules/user/index.ts
+++ src/modules/user/index.ts
@@ -1,15 +1,5 @@
-import { UserRepository } from './repositories/user.repository';
 import { User } from './entities/user.entity';
 import { UserModule } from './user.module';
-import { UserService } from './services/user.service';
-import { ResetPasswordToken } from './entities/reset-password-token.entity';
-import { ResetPasswordTokenService } from './services/reset-password-token.service';
+import { UserService } from './user.service';
 
-export {
-  User,
-  UserRepository,
-  UserModule,
-  UserService,
-  ResetPasswordToken,
-  ResetPasswordTokenService,
-};
+export { User, UserModule, UserService };
