8fac203a9592ded3d70da7e20d9fe6cd0aa45e68
majdshatti <majdshatti8@gmail.com>
2023-09-30 00:43:45 +0300

    feat: export exception factory

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,13 @@ import { formatResponse } from './http/response.util';
 import { isValidEmail } from './validators/is-email.util';
 import { isObjKey } from './data-structures/is-object-key.util';
 import { arrayify } from './data-structures/arrayify.util';
+import { exceptionFactory } from './exceptions/validation-factory.util';
 
-export { generateKey, formatResponse, isValidEmail, isObjKey, arrayify };
+export {
+  generateKey,
+  formatResponse,
+  isValidEmail,
+  isObjKey,
+  arrayify,
+  exceptionFactory,
+};
