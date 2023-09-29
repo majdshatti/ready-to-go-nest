864aefefaf41f33572fad9640c0014871d0891d5
majdshatti <majdshatti8@gmail.com>
2023-09-29 18:11:21 +0300

    feat: add validation pipes and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
-import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor, VersioningType } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
 import { WinstonModule } from 'nest-winston';
 import { loggerOptions } from './configs/logger.config';
-import { AllExceptionsFilter } from './exceptions';
-import { MailService } from './mail/mail.service';
-import { MailerService } from '@nestjs-modules/mailer';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -14,10 +20,24 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
+  app.useGlobalPipes(new ValidationPipe());
+
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Ready To Go Template')
+    .setDescription('The template API description')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3001);
 }
