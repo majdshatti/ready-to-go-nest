5f7575e5f41bac5550b29218499cb23760c2c24c
majdshatti <majdshatti8@gmail.com>
2023-09-29 21:31:57 +0300

    refactor: unit strategy interface

--- src/modules/auth/interfaces/strategy.interface.ts
+++ src/modules/auth/interfaces/strategy.interface.ts
@@ -4,18 +4,7 @@ import { ICredentials } from './credentials.interface';
 import { VerifyCallback } from 'passport-google-oauth20';
 import { Request } from 'express';
 
-export interface IJwtStrategy {
-  validate(payload: IPayload);
-  login(credentials: ICredentials);
-}
-
-export interface IGoogleStrategy {
-  validate(
-    accessToken: string,
-    refreshToken: string,
-    profile: any,
-    done: VerifyCallback,
-  );
-
-  login(request: Request);
+export interface IStrategy {
+  login(obj: ICredentials | Request);
+  validate(...args: any[]);
 }
