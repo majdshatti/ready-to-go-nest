916d9d29c748051b35b69819b6cc7320566b2d1e
majdshatti <majdshatti8@gmail.com>
2023-09-29 18:17:33 +0300

    feat: add internal error processor

---
+++ src/mail/consumers/error.consumer.ts
@@ -0,0 +1,28 @@
+import { Process, Processor } from '@nestjs/bull';
+import { Job } from 'bull';
+import { MailService } from '../mail.service';
+import { IMailError } from '../interfaces/error.interface';
+import { LogService } from 'src/modules/log';
+
+@Processor('error')
+export class ErrorMailConsumer {
+  constructor(
+    private readonly mailService: MailService,
+    private readonly logService: LogService,
+  ) {}
+
+  @Process('internal_error_occurred')
+  async sendException(job: Job<IMailError>) {
+    const error: IMailError = job.data;
+
+    await this.logService.save({
+      message: error.message,
+      statusCode: error.code,
+      path: error.path,
+    });
+
+    await this.mailService.sendException(error);
+
+    return {};
+  }
+}
