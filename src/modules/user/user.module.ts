9585a44efbb8e1b0d331e14a75f7af1d7533efe3
majdshatti <majdshatti8@gmail.com>
2023-09-30 20:31:47 +0300

    feat: add unique constraint to providers

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,11 +3,12 @@ import { UserController } from './controllers/user.controller';
 import { UserService } from './user.service';
 import { UserRepository } from './user.repository';
 import { FilterModule } from '../filter/filter.module';
+import { UniqueConstraint } from 'src/common/class-validator';
 
 @Module({
   imports: [FilterModule],
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, UniqueConstraint],
   exports: [UserService],
 })
 export class UserModule {}
